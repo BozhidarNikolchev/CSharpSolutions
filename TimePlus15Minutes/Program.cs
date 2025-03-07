682b7759859d950eb69f7851bdecca632854c8e0
Bozhidar <nikolchevbobi4@gmail.com>
2025-03-07 20:40:40 +0100

    Add project files.

---
+++ TimePlus15Minutes/Program.cs
@@ -0,0 +1,26 @@
+﻿int hour = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+if (minutes >= 45)
+{
+    hour = hour + 1;
+    minutes = (minutes + 15) % 60;
+}
+else
+{
+    minutes = minutes + 15;
+}
+
+if (hour == 24)
+{
+    hour = 0;
+}
+
+if (minutes < 10)
+{
+    Console.WriteLine($"{hour}:0{minutes}");
+}
+else
+{
+    Console.WriteLine($"{hour}:{minutes}");
+}
