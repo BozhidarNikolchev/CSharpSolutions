d933b7828cb773c0d632e5b23d6edaf58299580d
Bozhidar <nikolchevbobi4@gmail.com>
2025-03-08 22:01:48 +0100

    Add project files.

---
+++ GodzillavsKong/Program.cs
@@ -0,0 +1,26 @@
+﻿double movieBudget = double.Parse(Console.ReadLine());
+int extrasCount = int.Parse(Console.ReadLine());
+double clothingForAnExtra =  double.Parse(Console.ReadLine());
+
+double decorExpense = movieBudget * 0.10;
+double clothingExpense = extrasCount * clothingForAnExtra;
+
+if(extrasCount > 150)
+{
+    clothingExpense = clothingExpense - (clothingExpense * 0.10);
+}
+
+double totalExpenses = decorExpense + clothingExpense;
+
+double difference = Math.Abs(movieBudget - totalExpenses);
+
+if (totalExpenses > movieBudget)
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {difference:f2} leva more.");
+}
+else if (totalExpenses <= movieBudget)
+{
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {difference:f2} leva left.");
+}
