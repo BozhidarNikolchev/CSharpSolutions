f17dc8ca074095aa5cfe642f90dcbacbcf9d702e
Bozhidar <nikolchevbobi4@gmail.com>
2025-03-10 20:05:27 +0100

    Add project files.

---
+++ LunchBreak/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+string seriesName = Console.ReadLine();
+int episodeDuration = int.Parse(Console.ReadLine());
+int breakDuration = int.Parse(Console.ReadLine());
+
+double lunchDuration = (double)breakDuration * 1 / 8;
+double restDuration = (double)breakDuration * 1 / 4;
+
+double leftTime = breakDuration - lunchDuration - restDuration;
+
+double diff = Math.Abs(leftTime - episodeDuration);
+
+if (leftTime >= episodeDuration)
+{
+    Console.WriteLine($"You have enough time to watch {seriesName} and left with {Math.Ceiling(diff)} minutes free time.");
+}
+else
+{
+    Console.WriteLine($"You don't have enough time to watch {seriesName}, you need {Math.Ceiling(diff)} more minutes.");
+}
