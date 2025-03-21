c7dcac614551b0236307be5fc755a27cc41c70e0
Bozhidar <nikolchevbobi4@gmail.com>
2025-03-21 22:17:25 +0100

    Add project files.

---
+++ PersonalTitles/Program.cs
@@ -0,0 +1,25 @@
+﻿double age = double.Parse(Console.ReadLine());
+char gender = char.Parse(Console.ReadLine());
+
+string output = "";
+
+if (age < 16)
+{
+    if (gender == 'm')
+        output = "Master";
+
+    else if (gender == 'f')
+        output = "Miss";
+
+}
+else
+{
+    if (gender == 'm')
+        output = "Mr.";
+
+    else if (gender == 'f')
+        output = "Ms.";
+
+}
+
+Console.WriteLine(output);
