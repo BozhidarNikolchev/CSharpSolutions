d37ba0310a0388d249efa0f3fda75c912cf1ad98
Bozhidar <nikolchevbobi4@gmail.com>
2025-06-06 21:02:13 +0200

    Add project files.

---
+++ Ages/Program.cs
@@ -0,0 +1,22 @@
+﻿int inputAge = int.Parse(Console.ReadLine());
+
+if (inputAge >= 0 && inputAge <= 2)
+{
+    Console.WriteLine("baby");
+}
+else if (inputAge <= 13)
+{
+    Console.WriteLine("child");
+}
+else if (inputAge <= 19)
+{
+    Console.WriteLine("teenager");
+}
+else if (inputAge <= 65)
+{
+    Console.WriteLine("adult");
+}
+else if (inputAge >= 66)
+{
+    Console.WriteLine("elder");
+}
---
+++ Login/Program.cs
@@ -0,0 +1,28 @@
+﻿string userName = Console.ReadLine();
+
+for (int i = 0; i < 4; i++)
+{
+    string password = Console.ReadLine();
+
+    char[] charArray = password.ToCharArray();
+    Array.Reverse(charArray);
+    string reversedPassword = new string(charArray);
+
+    if (reversedPassword == userName)
+    {
+        Console.WriteLine($"User {userName} logged in.");
+        return;
+    }
+
+    if (i == 3)
+    {
+        Console.WriteLine($"User {userName} blocked!");
+        return;
+    }
+
+    if (reversedPassword != userName)
+    {
+        Console.WriteLine("Incorrect password. Try again.");
+    }
+
+}
