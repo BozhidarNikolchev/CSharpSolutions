a1953b3fa2ba529e066454a7cffe441b123026c3
Bozhidar <nikolchevbobi4@gmail.com>
2025-06-08 21:06:26 +0200

    Add project files.

---
+++ Orders/Program.cs
@@ -0,0 +1,21 @@
+﻿int ordersCount = int.Parse(Console.ReadLine());
+
+int counterOfOrders = 0;
+decimal total = 0;
+
+while (counterOfOrders < ordersCount)
+{
+    decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    int capsulesCount = int.Parse(Console.ReadLine());
+
+    decimal price = (days * capsulesCount) * pricePerCapsule;
+    Console.WriteLine($"The price for the coffee is: ${price:f2}");
+
+    total += price;
+
+    counterOfOrders++;
+}
+
+Console.WriteLine($"Total: ${total:f2}");
+
