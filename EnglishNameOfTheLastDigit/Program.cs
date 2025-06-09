f06ee65f453a8915480f4f9432653cf5eccca7a9
Bozhidar <nikolchevbobi4@gmail.com>
2025-06-09 21:14:29 +0200

    Add project files.

---
+++ EnglishNameOfTheLastDigit/Program.cs
@@ -0,0 +1,31 @@
+﻿string inputNumber = Console.ReadLine();
+
+char lastDigit = inputNumber[inputNumber.Length - 1];
+string spellingResult = GetDigitName(lastDigit);
+Console.WriteLine(spellingResult);
+
+static string GetDigitName(char lastDigit)
+{
+    if (lastDigit == '0')
+        return "zero";
+    else if (lastDigit == '1')
+        return "one";
+    else if (lastDigit == '2')
+        return "two";
+    else if (lastDigit == '3')
+        return "three";
+    else if (lastDigit == '4')
+        return "four";
+    else if (lastDigit == '5')
+        return "five";
+    else if (lastDigit == '6')
+        return "six";
+    else if (lastDigit == '7')
+        return "seven";
+    else if (lastDigit == '8')
+        return "eight";
+    else if (lastDigit == '9')
+        return "nine";
+    else
+        return "Not a digit";
+}
---
+++ SortNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿int biggestNum = 0;
+int secondBiggestNum = 0;
+int thirdBiggestNum = 0;
+
+int[] numbers = new int[3];
+
+for (int i = 0; i < 3; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+
+    numbers[i] = currentNumber;
+}
+
+Array.Sort(numbers);
+Array.Reverse(numbers);
+
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
