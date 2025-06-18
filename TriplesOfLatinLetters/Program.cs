7f5ee851819ee9b47c779654192be3bff949eb3b
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-06-18 17:20:35 +0200

    Add project files.

---
+++ TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,15 @@
+﻿int inputInteger = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < inputInteger; i++)
+{
+    char firstChar = (char)('a' + i);
+    for (int j = 0; j < inputInteger; j++)
+    {
+        char secondChar = (char)('a' + j);
+        for (int k = 0; k < inputInteger; k++)
+        {
+            char thirdChar = (char)('a' + k);
+            Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+        }
+    }
+}
