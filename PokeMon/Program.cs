9cf7a60fd19da7dfc04c6fbd87d20f6b883476d2
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-06-23 20:00:58 +0200

    Add project files.

---
+++ PokeMon/Program.cs
@@ -0,0 +1,28 @@
+﻿int initialPower = int.Parse(Console.ReadLine());
+int targetDistance = int.Parse(Console.ReadLine());
+int exhaustionFactor = int.Parse(Console.ReadLine());
+
+int originalValue = initialPower;
+int pokedTargetsCount = 0;
+
+while (initialPower >= targetDistance)
+{
+    if (initialPower == originalValue / 2)
+    {
+        if (exhaustionFactor != 0)
+        {
+            initialPower /= exhaustionFactor;
+
+            if (initialPower < targetDistance)
+            {
+                break;
+            }
+        }
+
+    }
+
+        initialPower -= targetDistance;
+        pokedTargetsCount++;
+}
+
+Console.WriteLine($"{initialPower}\n{pokedTargetsCount}");
