84485f3fa809959c9437eb2e78adc0c67ac8600f
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-06-25 22:05:45 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿// Where - is used to check a given condition. It is part of LINQ.
+
+//double a = 0.1;
+//double b = 0.1;
+//bool check = false;
+
+//double eps = 0.000001;
+//if (Math.Abs(a - b)  < eps)
+//{
+//    check = true;
+//}
+
+//Console.WriteLine(check);
+
+int a = int.Parse(Console.ReadLine());
+
+int[] array = a.ToString().Where(char.IsDigit).Select(d => d - '0').ToArray();
+
+foreach (int number in array)
+{
+    Console.WriteLine(number);
+}
---
+++ FromLeftToTheRight/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        int numberOfLines = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfLines; i++)
+        {
+            BigInteger[] input = Console.ReadLine()
+                                        .Split()
+                                        .Select(BigInteger.Parse)
+                                        .ToArray();
+
+            BigInteger leftNumber = input[0];
+            BigInteger rightNumber = input[1];
+
+            BigInteger biggerNumber = leftNumber > rightNumber ? leftNumber : rightNumber;
+
+            int sum = CalculateDigitSum(biggerNumber);
+            Console.WriteLine(sum);
+        }
+    }
+
+    static int CalculateDigitSum(BigInteger number)
+    {
+        return number.ToString()
+                     .Where(char.IsDigit) 
+                     .Select(d => d - '0') 
+                     .Sum();
+    }
+}
