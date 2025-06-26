95caa78aa52bf3551ed272bca7402304c4391de9
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-06-26 21:56:30 +0200

    Add project files.

---
+++ BalancedBrackets/Program.cs
@@ -0,0 +1,32 @@
+﻿short counter = short.Parse(Console.ReadLine());
+
+short balance = 0;
+
+for (int i = 0; i < counter; i++)
+{
+    string inputData = Console.ReadLine();
+
+    if (inputData == "(")
+    {
+        balance++;
+    }
+    else if (inputData == ")")
+    {
+        balance--;
+    }
+
+    if (balance < 0)
+    {
+        Console.WriteLine("UNBALANCED");
+        return; 
+    }
+}
+
+if (balance == 0)
+{
+    Console.WriteLine("BALANCED");
+}
+else
+{
+    Console.WriteLine("UNBALANCED");
+}
