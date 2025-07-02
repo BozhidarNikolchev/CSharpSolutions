9d2f5daced244eca899dd68d17452290df92e0cd
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-07-02 20:30:10 +0200

    Add project files.

---
+++ KaminoFactory/Program.cs
@@ -0,0 +1,47 @@
+﻿int sequencesLength = int.Parse(Console.ReadLine());
+string currentSequence;
+
+int bestSumOfOnes = 0;
+int bestSequenceNumber = 0;
+int indexOfBestSequence = 100;
+
+while ((currentSequence = Console.ReadLine()) != "Clone them!")
+{
+    int[] currentSequenceToArray = currentSequence.Split("!", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    bestSequenceNumber++;
+
+    int sumOfOnes = 0;
+    foreach (int number in currentSequenceToArray)
+    {
+        if (number == 1)
+        {
+            sumOfOnes++;
+        }
+    }
+
+    int currentIndexOfSequence = 0;
+    for (int i = 0; i < currentSequenceToArray.Length; i++)
+    {
+        if (currentSequenceToArray[i] == 1)
+        {
+            currentIndexOfSequence += i;
+        }
+    }
+
+   
+
+
+    if (sumOfOnes > bestSumOfOnes)
+    {
+        bestSumOfOnes = sumOfOnes;
+        indexOfBestSequence = currentIndexOfSequence;
+    }
+
+}
+
+/*
+5
+1!0!1!1!0
+0!1!1!0!0
+Clone them!
+*/
---
+++ MagicSum/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int targetNumber = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < inputNumbers.Length - 1; i++)
+{
+    for (int j = i + 1; j < inputNumbers.Length; j++)
+    {
+        if (inputNumbers[i] + inputNumbers[j] == targetNumber)
+        {
+            Console.WriteLine($"{inputNumbers[i]} {inputNumbers[j]}");
+        }
+    }
+}
