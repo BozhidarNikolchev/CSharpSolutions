2423c25443fdc205c249211b0505bff22305f2b5
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-07-18 18:59:29 +0200

    Add project files.

---
+++ MathOperations/Program.cs
@@ -0,0 +1,28 @@
+﻿int firstNum = int.Parse(Console.ReadLine());
+char sign = char.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine(CalculateAccordingToSign(firstNum, sign, secondNum));
+static int CalculateAccordingToSign(int firstNum, char sign, int secondNum)
+{
+    int result = 0;
+
+    if (sign == '*')
+    {
+        result = firstNum * secondNum;
+    }
+    else if (sign == '/')
+    {
+        result = firstNum / secondNum;
+    }
+    else if (sign == '+')
+    {
+        result = firstNum + secondNum;
+    }
+    else if (sign == '-')
+    {
+        result = firstNum - secondNum;
+    }
+
+    return result;
+}
---
+++ MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,42 @@
+﻿int inputNum = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int[] digits = inputNum.ToString().Select(digit => int.Parse(digit.ToString())).ToArray();
+
+int evenDigits = GetSumOfEvenDigits(digits);
+int oddDigits = GetSumOfOddDigits(digits);
+
+Console.WriteLine(GetMultipleOfEvenAndOdds(evenDigits, oddDigits));
+static int GetMultipleOfEvenAndOdds(int evenDigits, int oddDigits)
+{
+    return evenDigits * oddDigits;
+}
+
+static int GetSumOfEvenDigits(int[] digits)
+{
+    int evenDigits = 0;
+
+    for (int i = 0; i < digits.Length; i++)
+    {
+        if (digits[i] % 2 == 0)
+        {
+            evenDigits += digits[i];
+        }
+    }
+
+    return evenDigits;
+}
+
+static int GetSumOfOddDigits(int[] digits)
+{
+    int oddDigits = 0;
+
+    for (int i = 0; i < digits.Length; i++)
+    {
+        if (digits[i] % 2 != 0)
+        {
+            oddDigits += digits[i];
+        }
+    }
+
+    return oddDigits;
+}
