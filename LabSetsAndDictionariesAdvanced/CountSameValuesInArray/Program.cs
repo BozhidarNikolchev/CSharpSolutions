e27531669cfe62b865ef79e0c2c384cb8a93a895
Bozhidar <nikolchevbobi4@gmail.com>
2025-11-18 20:07:52 +0100

    add new code

--- LabSetsAndDictionariesAdvanced/CountSameValuesInArray/Program.cs
+++ LabSetsAndDictionariesAdvanced/CountSameValuesInArray/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var inputValues = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(double.Parse)
+            .ToArray();
+
+        var valuesCounts = new Dictionary<double, int>();
+
+        foreach (var currentNumber in inputValues)
+        {
+            if (!valuesCounts.ContainsKey(currentNumber))
+            {
+                valuesCounts[currentNumber] = 0;
+            }
+
+            valuesCounts[currentNumber]++;
+        }
+
+        foreach (var entry in valuesCounts)
+        {
+            var key = entry.Key;
+            var value = entry.Value;
+
+            Console.WriteLine($"{key} - {value} times");
+        }
     }
 }
--- LabSetsAndDictionariesAdvanced/Demo/Program.cs
+++ LabSetsAndDictionariesAdvanced/Demo/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
+
+        // var phoneNumbers = new Dictionary<string, string>();
+
+        // var names = new Dictionary<string, double>();
+        // names["Maria"] = 4.5;
+        // names["Maria"] = 6.3;
+        // names["Pepi"] = 5.2;
+        // names["Gogi"] = 6.3;
+
+        // Console.WriteLine(string.Join(" ", names.Values));
+
+        // foreach (var person in  names)
+        // {
+        //     string name = person.Key;
+        //     double grade = person.Value;
+        //
+        //     Console.WriteLine($"{name} -> {grade}");
+        // }
+
+        // List<int> nums = new List<int>(){ 1, 32, 59, 6, 700, 9 };
+        //
+        // nums = nums.OrderBy(num => num).ToList();
+        // Console.WriteLine(string.Join(", ", nums));
+        //
+        // var result  = nums.OrderByDescending(num => num).ToList();
+        // Console.WriteLine(string.Join(", ", nums));
+
+        // var names = new Dictionary<string, double>();
+        // names["Maria"] = 4.5;
+        // names["Maria"] = 6.3;
+        // names["Pepi"] = 5.2;
+        // names["Gogi"] = 6.3;
+
         
-    }
+        //nestedDictionary 
+        // var states = new Dictionary<string, Dictionary<string, int>>();
+        //
+        // states["US"] = new Dictionary<string, int>();
+        // states["US"]["Chicago"] = 200000;
+        
+        
+
+    } 
 }
