83d2307bf2aa0ef10cbdfe7bfcb7f5e40b9c5e58
BozhidarNikolchev <bozhidar650@gmail.com>
2025-12-10 07:47:37 +0100

    add modification

--- ExercisesSetsAndDictionariesAdvanced/UniqueUsernames/Program.cs
+++ ExercisesSetsAndDictionariesAdvanced/UniqueUsernames/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int usernamesCount = int.Parse(Console.ReadLine());
+
+var uniqueUsers = new HashSet<string>();
+
+for (int i = 0; i < usernamesCount; i++)
+{
+    string currentUser = Console.ReadLine();
+
+    uniqueUsers.Add(currentUser);
+}
+
+foreach (var username in uniqueUsers)
+{
+    Console.WriteLine(username);
+}
