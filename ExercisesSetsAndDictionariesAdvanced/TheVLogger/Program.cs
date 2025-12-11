bc65bdb15cf1e0e1710af3e2898b0a8361f20657
BozhidarNikolchev <bozhidar650@gmail.com>
2025-12-11 21:49:35 +0100

    add vlogger project

---
+++ ExercisesSetsAndDictionariesAdvanced/TheVLogger/Program.cs
@@ -0,0 +1,47 @@
+﻿var vloggersInfo = new Dictionary<string, (HashSet<string>followers, HashSet<string>following)>();
+
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "Statistics")
+{
+    string[] action = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    string vloggerName = action[0];
+    string idol;
+
+    if (action.Length == 4 && action[1] == "joined")
+    {
+        if (!vloggersInfo.ContainsKey(vloggerName))
+            vloggersInfo[vloggerName] = (new HashSet<string>(), new HashSet<string>());
+        // vloggersInfo[vloggerName] = (followers: [], following: []);
+    }
+
+    if (action.Length == 3 && action[1] == "followed")
+    {
+        idol = action[2];
+        if (vloggersInfo.ContainsKey(vloggerName) && vloggersInfo.ContainsKey(idol)
+                                                  && vloggerName != idol)
+        {
+            vloggersInfo[vloggerName].following.Add(idol);
+            vloggersInfo[idol].followers.Add(vloggerName);
+        }
+    }
+}
+
+Console.WriteLine($"The V-Logger has a total of {vloggersInfo.Count} vloggers in its logs.");
+
+int order = 1;
+foreach ((string vlogger, (HashSet<string> followers, HashSet<string> following)) in vloggersInfo
+             .OrderByDescending(x => x.Value.followers.Count)
+             .ThenBy(x => x.Value.following.Count))
+{
+    Console.WriteLine($"{order}. {vlogger} : {followers.Count} followers, {following.Count} following");
+
+    if (order == 1)
+    {
+        foreach (string follower in followers.Order())
+        {
+            Console.WriteLine($"*  {follower}");
+        }
+    }
+
+    order++;
+}
