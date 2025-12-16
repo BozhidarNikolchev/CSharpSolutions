65ff407309f3674512e284b6112cd70eb2ba32f9
BozhidarNikolchev <bozhidar650@gmail.com>
2025-12-16 13:35:56 +0100

    add all

---
+++ StreamsFilesAndDirectories/Playground/Program.cs
@@ -0,0 +1,24 @@
+﻿using var reader = new StreamReader(@".\Data\Input.txt");
+using var writer = new StreamWriter(@".\Data\Output.txt", true);
+
+    int counter = 1;
+
+    while (true)
+    {
+        var line = reader.ReadLine();
+
+        if (line == null)
+        {
+            break;
+        }
+
+        writer.WriteLine($"{counter}. {line}");
+        counter++;
+    }
+
+Console.WriteLine("Done");
+
+
+// . -> Current directory
+// .. -> Parent directory
+
