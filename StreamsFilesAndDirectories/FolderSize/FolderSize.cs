ce1c58b26659ef42df8d6391c67b46f504b809cc
BozhidarNikolchev <bozhidar650@gmail.com>
2026-01-05 19:41:20 +0100

    add last

--- StreamsFilesAndDirectories/FolderSize/FolderSize.cs
+++ StreamsFilesAndDirectories/FolderSize/FolderSize.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
+
     public class FolderSize
     {
         static void Main(string[] args)
@@ -14,6 +16,17 @@
 
         public static void GetFolderSize(string folderPath, string outputFilePath)
         {
+            long totalBytes = 0;
+
+            foreach (var file in Directory.GetFiles(folderPath, "*", SearchOption.AllDirectories))
+            {
+                totalBytes += new FileInfo(file).Length;
+            }
+
+            double totalKB = totalBytes / 1024.0;
+
+            string result = $"{totalKB.ToString()} KB";
+            File.WriteAllText(outputFilePath, result);
         }
     }
 }
